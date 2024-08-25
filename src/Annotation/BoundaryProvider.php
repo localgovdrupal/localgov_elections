ff0b89b0d9f1080edc107537cb4e496e5a8150ba
Lee Mills <8024370+millnut@users.noreply.github.com>
2024-08-25 14:21:26 +0100

    fix: test errors

--- src/Annotation/BoundaryProvider.php
+++ src/Annotation/BoundaryProvider.php
@@ -9,40 +9,47 @@ use Drupal\Component\Annotation\Plugin;
 /**
  * Defines boundary_provider annotation object.
  *
+ * @see plugin_api
+ *
  * @Annotation
  */
 class BoundaryProvider extends Plugin {
 
   /**
-   * Constructor.
+   * The plugin ID.
+   *
+   * @phpstan-ignore-next-line This is allowed in Drupal, but PHPStan complains even though this is valid
    */
-  public function __construct(
-    /**
-     * The plugin ID.
-     */
-    public readonly string $id,
-    /**
-     * The human-readable name of the plugin.
-     *
-     * @ingroup plugin_translatable
-     */
-    public readonly string $title,
-    /**
-     * The description of the plugin.
-     *
-     * @ingroup plugin_translatable
-     */
-    public readonly string $description,
-    /**
-     * An array of plugin provided forms.
-     *
-     * Returns an array of plugin provided forms. We only support the download
-     * form at the moment.
-     *
-     * @returns array
-     */
-    public array $form = [],
-  ) {
-  }
+  public readonly string $id;
+
+  /**
+   * The human-readable name of the plugin.
+   *
+   * @ingroup plugin_translatable
+   *
+   * @phpstan-ignore-next-line This is allowed in Drupal, but PHPStan complains even though this is valid
+   */
+  public readonly string $title;
+
+  /**
+   * The description of the plugin.
+   *
+   * @ingroup plugin_translatable
+   *
+   * @phpstan-ignore-next-line This is allowed in Drupal, but PHPStan complains even though this is valid
+   */
+  public readonly string $description;
+
+  /**
+   * An array of plugin provided forms.
+   *
+   * Returns an array of plugin provided forms. We only support the download
+   * form at the moment.
+   *
+   * @returns array
+   *
+   * @phpstan-ignore-next-line This is allowed in Drupal, but PHPStan complains even though this is valid
+   */
+  public array $form = [];
 
 }
