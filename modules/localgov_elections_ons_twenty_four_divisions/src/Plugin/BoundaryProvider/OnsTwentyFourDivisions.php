58c6c2cb30bdfe89dece8791993eeb6413416b74
Alistair <Alistair.Wearring@west-lindsey.gov.uk>
2025-05-08 17:43:41 +0100

    Translatable strings must not begin or end with white spaces

--- modules/localgov_elections_ons_twenty_four_divisions/src/Plugin/BoundaryProvider/OnsTwentyFourDivisions.php
+++ modules/localgov_elections_ons_twenty_four_divisions/src/Plugin/BoundaryProvider/OnsTwentyFourDivisions.php
@@ -132,7 +132,7 @@ class OnsTwentyFourDivisions extends BoundaryProviderPluginBase implements Conta
       '#title' => "Local Authority County Code (CTY24CD)",
       '#maxlength' => 1000,
       '#default_value' => $this->configuration['cty'] ?? "",
-      '#description' => $this->t("County code. You can find this ") . "<a href='$cty_url'>" . $this->t("here") . "</a>" . $this->t(". Use the value from the CTY24CD column."),
+      '#description' => $this->t("County code. You can find this") . " <a href='$cty_url'>" . $this->t("here") . "</a>. " . $this->t("Use the value from the CTY24CD column."),
       '#required' => TRUE,
     ];
 
@@ -141,7 +141,7 @@ class OnsTwentyFourDivisions extends BoundaryProviderPluginBase implements Conta
       '#title' => "Local Authority District Code (LAD23CD)",
       '#maxlength' => 1000,
       '#default_value' => $this->configuration['lad'] ?? "",
-      '#description' => $this->t("Local Authority District code. You can find this ") . "<a href='$lad_url'>" . $this->t("here") . "</a>" . $this->t(". Use the value from the LAD23CD column."),
+      '#description' => $this->t("Local Authority District code. You can find this") . " <a href='$lad_url'>" . $this->t("here") . "</a>. " . $this->t("Use the value from the LAD23CD column."),
       '#required' => FALSE,
     ];
 
