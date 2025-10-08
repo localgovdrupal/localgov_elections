3289304a08303beda444c61b43994475bc0ff597
Dan Champion <dan@rohallion.agency>
2025-10-08 15:28:43 +0100

    Allow duplicated election to be re-titled, set new election and areas to be unpublished

--- src/Service/ElectionDuplicator.php
+++ src/Service/ElectionDuplicator.php
@@ -27,19 +27,22 @@ class ElectionDuplicator {
    *
    * @param \Drupal\node\Entity\Node $election
    *   The original election node.
+   * @param string $new_title
+   *   The title for the new election.
    *
    * @return \Drupal\node\Entity\Node
    *   The newly created election node.
    */
-  public function duplicateElection(Node $election): Node {
+  public function duplicateElection(Node $election, string $new_title): Node {
     if ($election->bundle() !== 'localgov_election') {
       throw new \InvalidArgumentException('Node must be a localgov_election.');
     }
 
     // Clone the election node itself.
     $new_election = $election->createDuplicate();
-    $new_election->setTitle($election->getTitle() . ' (Copy)');
+    $new_election->setTitle($new_title);
     $new_election->setUnpublished();
+    $new_election->setChangedTime(time());
     $new_election->save();
 
     // Load area votes linked to this election.
@@ -49,7 +52,7 @@ class ElectionDuplicator {
     ]);
 
     foreach ($area_votes as $area_vote) {
-      $this->duplicateAreaVote($area_vote, $new_election->id());
+      $this->duplicateAreaVote($area_vote, $new_election->id(), $election->getTitle(), $new_title);
     }
 
     return $new_election;
@@ -57,11 +60,35 @@ class ElectionDuplicator {
 
   /**
    * Duplicate a localgov_area_vote node and its paragraphs.
+   *
+   * @param \Drupal\node\Entity\Node $area_vote
+   *   The area vote to duplicate.
+   * @param int $new_election_id
+   *   The ID of the new election.
+   * @param string $original_election_title
+   *   The title of the original election.
+   * @param string $new_election_title
+   *   The title of the new election.
+   *
+   * @return \Drupal\node\Entity\Node
+   *   The duplicated area vote node.
    */
-  protected function duplicateAreaVote(Node $area_vote, int $new_election_id): Node {
+  protected function duplicateAreaVote(Node $area_vote, int $new_election_id, string $original_election_title, string $new_election_title): Node {
     $new_area_vote = $area_vote->createDuplicate();
     // Point to new election.
     $new_area_vote->set('localgov_election', $new_election_id);
+
+    // Update area title to reflect new election title.
+    $original_area_title = $area_vote->getTitle();
+    $new_area_title = str_replace($original_election_title, $new_election_title, $original_area_title);
+    $new_area_vote->setTitle($new_area_title);
+
+    // Set as unpublished and update the changed timestamp to current time.
+    $new_area_vote->setUnpublished();
+    $new_area_vote->setChangedTime(time());
+
+    // Clear the winner field - it will reference old paragraph IDs.
+    $new_area_vote->set('localgov_election_winner', []);
 
     // Duplicate paragraphs.
     $paragraph_fields = [
